73130dfa8f657b520c81a2901c9196e1699495f1
icarusxx <42697793+icarusxx@users.noreply.github.com>
2018-08-26 18:46:59 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title> MD ASIM</title><link rel="stylesheet" type="text/css" href="http://flambon.xtgem.com/fmb/simple_v4-7.css" media="all,handheld"/><link rel="shortcut icon" href="">
+
 <?php
 function auto(){
 		echo' <center>SELAMAT DATANG DI BOOT OY<p>
-Untuk melakukan bot. kalian wajib donasi
+
+		Untuk melakukan bot. kalian wajib donasi
 </center>
 ';
 }
